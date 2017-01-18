12f2fdf8ac05095217f2ba196da4c5f8710ce390
Ivan5646 <Mike000027@yandex.ru>
2017-01-19 02:07:30 +0600

    posts

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 angular.module('flapperNews', ['ui.router'])
 
 .config(['$stateProvider', '$urlRouterProvider',
-function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state('home', {
-      url: '/home',
-      templateUrl: '/home.html',
-      controller: 'MainCtrl'
+  function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+      })
+      .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
     });
-  $urlRouterProvider.otherwise('home');
+    $urlRouterProvider.otherwise('home');
 }])
 
 .factory('posts', [function(){ 
@@ -18,21 +23,28 @@ function($stateProvider, $urlRouterProvider) {
   return o;
 }])
 
-.controller('MainCtrl', ['$scope', 'posts', // posts is a factotry
-function($scope, posts){
-  $scope.posts = posts.posts; // Bind the $scope.posts variable in our controller to the posts array in our service
+.controller('MainCtrl', ['$scope', 'posts', // posts is a factory
+  function($scope, posts){
+    $scope.posts = posts.posts; // Bind the $scope.posts variable in our controller to the posts array in our service
 
-  $scope.addPost = function(){
-    if(!$scope.title || $scope.title === '') { return; } // prevents blank field submtting
-    $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0}); // $scope.title allows to get user's input
-    $scope.title = ''; // clears the input
-    $scope.link = ''; // clears the link input
-  };
-  $scope.incrementUpvotes = function(post) {
-    post.upvotes += 1;
-  };
-
-}
+    $scope.addPost = function(){
+      if(!$scope.title || $scope.title === '') { return; } // prevents blank field submtting
+      $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0, comments:[
+        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}]
+  }); // $scope.title allows to get user's input
+      $scope.title = ''; // clears the input
+      $scope.link = ''; // clears the link input
+    };
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    };
+  }
+])
+.controller('PostsCtrl', ['$scope','$stateParams','posts',
+  function($scope, $stateParams, posts){
+    $scope.post = posts.posts[$stateParams.id];
+  }
 ]);
 
 
